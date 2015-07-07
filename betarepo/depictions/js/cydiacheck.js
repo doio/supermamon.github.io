d714c7549bcc0c8aedf37dc64675ff6fbc94fae9
Raymond Velasquez <monvelasquez@gmail.com>
2015-07-07 17:21:11 +0800

    Create betarepo

---
+++ betarepo/depictions/js/cydiacheck.js
@@ -0,0 +1,3 @@
+// Cydia Check
+if (navigator.userAgent.search(/Cydia/) != -1)
+	document.write('<style type="text/css"> body { background: none !important; } .header { display: none !important; } </style>');
