015e3fd83df5f8710a058ce69e42a3ca5ac78872
Raymond Velasquez <monvelasquez@gmail.com>
2015-07-08 16:33:27 +0800

    /supermamon/ repo. sync with betarepo-nightly-15.07.08-1

--- repo/depictions/js/ios_version_check.js
+++ repo/depictions/js/ios_version_check.js
@@ -1,16 +1,17 @@
 /*
-- hopefully guys from http://beta.tmnlsthrn.com won't mind me using this. 
-- experimenting on hosting a personal repo.
-- this looks really nice on cydia
+I saw some parts of this code on the internet. I forgot where. If it's yours
+let me know and I'll credit you.
+
 */
+
 const VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f60a;";
 const VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
 const VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
 const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
 
-(function(document) {
-	"use strict";
-
+function ios_version_check(minIOS,maxIOS,otherIOS,selector,failClass) {
+	"use strict";	
+	
 	function parseVersionString(version) {
 		var bits = version.split(".");
 		return [ bits[0], bits[1] ? bits[1] : 0, bits[2] ? bits[2] : 0 ];
@@ -37,32 +38,30 @@ const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
 		}
 
 		return 0;
-	}
-
-	var prerequisite = document.querySelector(".prerequisite"),
-		version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
-
-	if (!prerequisite || !version) {
-		return;
+	}	
+	
+	var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
+	if (!version) {
+		return 0;
 	}
 
 	var osVersion = [ version[2], version[3], version[4] ? version[5] : 0 ],
 
 		osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
-		minString = prerequisite.dataset.minIos,
-		maxString = prerequisite.dataset.maxIos,
+		minString = minIOS,
+		maxString = maxIOS,
 
 		minVersion = parseVersionString(minString),
 		maxVersion = maxString ? parseVersionString(maxString) : null,
 
 		message = VERSION_CHECK_SUPPORTED,
 		isBad = false;
-
+		
 	if (compareVersions(minVersion, osVersion) == 1) {
 		message = VERSION_CHECK_NEEDS_UPGRADE.replace("%s", minString);
 		isBad = true;
 	} else if (maxVersion && compareVersions(maxVersion, osVersion) == -1) {
-		if ("unsupported" in prerequisite.dataset) {
+		if ("unsupported" == otherIOS) {
 			message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
 		} else {
 			message = VERSION_CHECK_UNCONFIRMED.replace("%s", osString);
@@ -71,10 +70,12 @@ const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
 		isBad = true;
 	}
 
-//	prerequisite.querySelector("p").textContent = message;
-    prerequisite.querySelector("p").innerHTML = message;
+    $(selector).html(message);
 
 	if (isBad) {
-		prerequisite.classList.add("info");
+		$(selector).addClass(failClass);
 	}
-})(document);
+	
+	return (isBad?-1:1)
+	
+}
