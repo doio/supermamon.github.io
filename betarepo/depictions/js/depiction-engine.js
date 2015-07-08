8960d835afdd8913b32482c85e5f351a7e7b91c4
Raymond Velasquez <monvelasquez@gmail.com>
2015-07-08 16:31:19 +0800

    betarepo-nightly-15.07.08-1

--- betarepo/depictions/js/depiction-engine.js
+++ betarepo/depictions/js/depiction-engine.js
@@ -1,48 +1,20 @@
-function run_depiction_engine(contentBlocks,xml) {
-	document.title = $(xml).find("package>name").text();
+function data_loader_engine(contentBlocks,xml) {
 
+	/* Loop through each if the contentBlocks */
 	$.each(contentBlocks, function (key,contentInfo){
 		
-		/*
-		if (key=='changesList') {
-			contentInfo.source=contentInfo.source.replace(/{VERSION}/i,version);
-		}
-		*/
 		console.log('Processing '+key);
 		console.log('  type= '+contentInfo.type);
+		
+		// go out if key element does not exits
+		if (!($(key).length)) {return}
+		
 		switch(contentInfo.type) {
-			case "list":
-				var list = $(xml).find(contentInfo.source);
-				if (list.size()==0) {
-					$(contentInfo.panel).hide(); //hide panel if 
-				} else {
-					if (!!contentInfo.reverseRender) {
-						list = $(list).get().reverse();
-					}
-					if (!key) {return}
-					$.each(list, function(index,value){
-						var item = $(value).text()
-						if (contentInfo.render) {
-							item = contentInfo.render(value);
-						}
-						if (!!contentInfo.reverseRender) {
-							$("#"+key).prepend('<li><p>'+item+'</p></li>')
-						} else {
-							$("#"+key).append('<li><p>'+item+'</p></li>')
-						}
-					});
-				}
-				break;
 			case "text":
-				if (!key) {return}
 				var content = $(xml).find(contentInfo.source).text();
-				if (contentInfo.render) {
-					content = contentInfo.render($(xml).find(contentInfo.source))
-				}
-				$("#"+key).html(content)
+				$(key).html(content)
 				break;
 			case "link":
-				if (!key) {return}
 				console.log('  url= '+contentInfo.url);
 				console.log('  text= '+contentInfo.text);
 
@@ -50,38 +22,56 @@ function run_depiction_engine(contentBlocks,xml) {
 				var params = [];
 				if (contentInfo.params) {
 					$.each(contentInfo.params, function(){
-						console.log(this);
+						this[1] = escape(this[1]);
 						params[params.length] = this.join('=');
 					});
 				}
 				url = url+'?'+params.join('&');
-				$("#"+key).append( $("<a></a>")
+				$(key).append( $("<a></a>")
 					.attr("href",url)
 					.text(contentInfo.text)
 				 );
+				break;				
+			case "list":
+				var list = $(xml).find(contentInfo.source);
+				
+				
+				if (list.size()==0) {
+					if (contentInfo.emptyListCallback) {
+						contentInfo.emptyListCallback($(key))
+					}
+				} else {
+					if (!!contentInfo.reverseRender) {
+						list = $(list).get().reverse();
+					}
+					$.each(list, function(index,value){
+						var item = $(value).text()
+						
+						if (!!contentInfo.reverseRender) {
+							$(key).prepend( $(contentInfo.paragraphElement).html("<p>"+item+"</p>") )
+						} else {
+							$(key).append( $(contentInfo.paragraphElement).html("<p>"+item+"</p>") )
+						}
+					});
+				}
 				break;
-			case "article":
-				if (!key) {return}
-				var articles = 	$(xml).find(contentInfo.source);
-				$.each(articles.get().reverse(),function(index,article){
-					console.log(article);
-					var title = $(article).find(contentInfo.titleSource).text();
-					console.log(title);
-					$(key).append("<h2>"+title+"</h2>");
-					var plist = $(key).append("<ul></ul>");
-					$.each($(article).find(contentInfo.paragraphSource),function(index,paragraph){
-						$(key+">ul").append( $("<li><p>"+$(paragraph).text()+"</p></li>") )
-					})
-
-				})
+			case "articles":
+				var articles = 	$(xml).find(contentInfo.source).children();
+				var titleID = 0;
+				$.each(articles,function(index,article){
+					var articleTitle = $(article).find(contentInfo.titleSource).text()
+					$(key).append( $(contentInfo.titleElement).html(articleTitle));
+					var container = $(contentInfo.paragraphContainer).attr("id",++titleID);
+					$(key).append($(container));
+					$.each( $(article).find(contentInfo.paragraphSource), function(index,paragraph){
+						$(container).append( $(contentInfo.paragraphElement).html("<p>"+$(paragraph).text()+"</p>") )
+					}) //paragraph
+				}) //article
 
 				break;
 			case "custom":
 				if (!key) {return}
-					console.log($(xml).find(contentInfo.source));
-
-				contentInfo.render( $('#'+key), $(xml).find(contentInfo.source) )
-
+				contentInfo.render( $(key), $(xml).find(contentInfo.source) )
 				break;
 		}//switch
 	}); //each
