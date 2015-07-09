c6a47d5699eac57844335c3c4c1a4606f229c9c2
Raymond Velasquez <monvelasquez@gmail.com>
2015-07-09 15:26:40 +0800

    betarepo-nightly-v15.09-1. use bootstrap framework

--- betarepo/depictions/js/cydiacheck.js
+++
@@ -1,3 +0,0 @@
-// Cydia Check
-if (navigator.userAgent.search(/Cydia/) != -1)
-	document.write('<style type="text/css"> body { background: none !important; } .header { display: none !important; } </style>');
